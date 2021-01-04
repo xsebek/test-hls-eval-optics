5f5bbbb0d8af99418732c200c701e0c73928df4d
Ondřej Šebek <xsebek@fi.muni.cz>
2021-01-04 14:33:28 +0100

    update for hls-0.8.0 and ghc-8.10.3

--- Main.hs
+++ Main.hs
@@ -4,14 +4,14 @@ import Data.Tuple.Optics (Field1 (_1))
 import Optics.Operators ((%~))
 
 -- >>> (\(a,b,c) -> (succ a,b,c)) (0,2,3)
--- (1,2,3)
+
 -- >>> (_1 %~ succ) (1, 2, 3)
---
+
 -- >>> someFunc
---
 someFunc :: (Int, Int, Int)
 someFunc = (_1 %~ succ) (1, 2, 3)
 
+-- >>> main
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
